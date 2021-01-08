3db4d9049b4fefb5ee795e79c5055fb538331856
Rajiv <Rajiv@DESKTOP-NVPTPKF>
2021-01-08 15:30:39 +0530

    forgotpassword file has beeen added

---
+++ src/test/java/com/test/facebook/ForgotPassword.java
@@ -0,0 +1,8 @@
+package com.test.facebook;
+import org.testng.annotations.Test;
+public class ForgotPassword {
+	@Test
+	public void forgotpassword() {
+		System.out.println("forgotpassord");
+	}
+}
